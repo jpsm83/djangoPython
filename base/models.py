0f2a8be0b70ec92013119733609d4657756c5de1
Joao <jpsm83@hotmail.com>
2023-07-06 13:11:17 +0200

    templates

--- base/models.py
+++ base/models.py
@@ -1,3 +1,45 @@
+# here we create our database tables
+
+# once the tables are ready use the command line to migrate
+# python manage.py makemigrations
+# it will create a migration file on this specific app
+# them
+# python manage.py migration
+# that will create the table on the project database
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    # relation with User
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # relation with Topic
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Message(models.Model):
+    # relation with User
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # relation with Room
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,23 +1,16 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
 # from django.http import HttpResponse
 
-rooms = [
-    {'id': 1, 'name': 'lets learn python'},
-    {'id': 2, 'name': 'code with me'},
-    {'id': 3, 'name': 'frontend developers'},
-]
-
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
