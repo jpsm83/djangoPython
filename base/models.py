111e69d01c4ce5679466bdc335f40bafddfe9279
Joao <jpsm83@hotmail.com>
2023-07-10 13:08:31 +0200

    profile page

--- base/models.py
+++ base/models.py
@@ -25,14 +25,15 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants =
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         # "-" means descendente, without means ascendent
         ordering = ['-updated', '-created']
-
+        
+    # next line of code is the "default" return in case any parameters is requested
     def __str__(self):
         return self.name
         
@@ -45,5 +46,10 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        # "-" means descendente, without means ascendent
+        ordering = ['-updated', '-created']
+
+    # next line of code is the "default" return in case any parameters is requested
     def __str__(self):
         return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -68,11 +68,13 @@ def home(request):
     # Q is a functionality from django.db.models that allow us to use "&" or "|" on the querys
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
-        Q(name__icontains=q)
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
     )
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -80,7 +82,8 @@ def room(request, pk):
     # 'model_set.all()' get all the data from the model parent with the model child
     # in this case 'message_set.all()' will enter in the parent MESSAGE
     # and bring back all messages relate to the child ROOM 
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
 # messages
     if request.method == 'POST':
         message = Message.objects.create(
@@ -88,10 +91,19 @@ def room(request, pk):
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
 
 # CRUD create
 @login_required(login_url='login')
@@ -128,9 +140,25 @@ def updateRoom(request, pk):
 @login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!')
+
     if request.method == "POST":
         room.delete()
         return redirect('home')
 
     return render(request, 'base/delete.html', {'obj': room})
 
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed to delete this message!')
+
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {'obj': message})
